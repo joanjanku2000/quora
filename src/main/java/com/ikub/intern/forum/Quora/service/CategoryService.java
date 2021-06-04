d177f636d6be57e14cf915aac4ca1f054176c3c7
joanjanku2000 <janku.joan4@gmail.com>
2021-06-04 23:38:56 +0200

    Category Logger

--- src/main/java/com/ikub/intern/forum/Quora/service/CategoryService.java
+++ src/main/java/com/ikub/intern/forum/Quora/service/CategoryService.java
@@ -9,6 +9,8 @@ import com.ikub.intern.forum.Quora.exceptions.BadRequestException;
 import com.ikub.intern.forum.Quora.repository.CategoryRepo;
 import com.ikub.intern.forum.Quora.utils.PageParams;
 import com.ikub.intern.forum.Quora.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,27 +24,35 @@ import java.util.Optional;
 public class CategoryService {
     @Autowired
     private CategoryRepo categoryRepo;
-
+    private static final Logger logger = LoggerFactory.getLogger(CategoryService.class);
     public void saveCategory(CategoryCreateRequest categoryCreateRequest, UserEntity createdBy) {
+        logger.info("Saving category {}",categoryCreateRequest);
         CategoryEntity categoryEntity ;
         if (categoryRepo.findByCategoryName(categoryCreateRequest.getName())!=null){
             throw new BadRequestException("Sorry, the category you are trying to add already exists");
         }
         categoryEntity = CategoryConverter.toEntity(categoryCreateRequest,createdBy);
          categoryRepo.save(categoryEntity);
+        logger.info("Saved category {}",categoryEntity);
     }
     public void deleteCategory(Long id){
+        logger.info("Deleting category {}",id);
         Optional<CategoryEntity> categoryEntity = categoryRepo.findById(id);
         if (!categoryEntity.isPresent()) throw new BadRequestException("The category you requested does not exist");
         categoryEntity.get().setActive(false);
         categoryRepo.save(categoryEntity.get());
+        logger.info("Deleted category {}",categoryEntity);
     }
     public List<CategoryDto> findAll(){
         return CategoryConverter.entityListToDtoList(categoryRepo.findAll());
     }
     public CategoryDto findById(Long id){
+        logger.info("Getting category {}",id);
         Optional<CategoryEntity> categoryEntity = categoryRepo.findById(id);
-        if (!categoryEntity.isPresent()) throw new BadRequestException("The category with the given id wasn;t found");
+        if (!categoryEntity.isPresent())
+            throw new BadRequestException("The category with the given id wasn;t found");
+        logger.info("Got category");
         return CategoryConverter.entityToDto(categoryEntity.get());
+
     }
 }
